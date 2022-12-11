b90dcd62174db5d6ef01799b0187ae13c803d085
dbaba2011 <dbaba2011@yahoo.com.com>
2022-12-11 10:55:47 -0800

    a script that compiles a C file and creates an executable named cisfun.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- * 		  puzzle, followed by a new line.
+ * 		  puzzle, followed by a new line
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	puts("\n"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
