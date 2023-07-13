e0d4e302110d1005fdef25b3a84f0e24bddcc0bd
dbaba2011 <dbaba2011@yahoo.com.com>
2023-07-13 08:43:49 -0700

    My 0x1E-serach_algorithms priject task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,66 +1,39 @@
 #include "search_algos.h"
+
 /**
- * binary_search- implementation of the binary search algorithim
- * @array: array to search
- * @size: size of array
- * @value: value to search
- * Return: index of value
- */
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int binary_search(int *array, size_t size, int value)
 {
-	int val;
+	size_t i, left, right;
 
 	if (array == NULL)
 		return (-1);
-	val = helper_binary(array, 0, size - 1, value);
-	return (val);
-}
 
-/**
- * helper_binary-helper function for binary search
- * @array: arrar to search
- * @left: pointer to left
- * @right: pointer to the right
- * @value: value to search for
- * Return: index of value
- */
-int helper_binary(int *array, size_t left, size_t right, int value)
-{
-	size_t mid = (right + left) / 2;
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-	if (left > right)
-		return (-1);
-	print_array(array, left, right);
-	if (array[mid] == value)
-		return (mid);
-	else if (value > array[mid])
-	{
-		left = mid + 1;
-		return (helper_binary(array, left, right, value));
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
 	}
-	else
-	{
-		right = mid - 1;
-		return (helper_binary(array, left, right, value));
-	}
+
 	return (-1);
-}
-/**
- * print_array - prints the array
- * @array: array to search]
- * @start: start of the array
- * @end: end of the array
- */
-void print_array(int *array, size_t start, size_t end)
-{
-	size_t i;
-
-	printf("Searching in array: ");
-	for (i = start; i <= end; i++)
-	{
-		printf("%d", array[i]);
-		if (i != end)
-			printf(", ");
-	}
-	printf("\n");
 }
